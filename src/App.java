87bfead492f319efc6a02eab57b7ba99a36c1cad
Alexsm111 <asm0083@alu.medac.es>
2024-04-25 15:38:08 +0200

    main2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
         System.out.println("Hello, main2!");
-=======
         System.out.println("Hello, cambio hotfix");
->>>>>>> hotfix
     }
 }
