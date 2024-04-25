3b9bea9c39fb1d96fc9304f78c890386ca0b942e
Alexsm111 <asm0083@alu.medac.es>
2024-04-25 15:51:03 +0200

    release2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, release!");
+        System.out.println("Hello, release2!");
     }
 }
